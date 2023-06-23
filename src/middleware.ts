62014d4afeca17f185cbb0e772ec50dba4d834fc
Mario Krstevski <mariokrstevski@hotmail.com>
2023-06-23 17:22:51 +0200

    Router

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware();
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
+// import { withClerkMiddleware } from "@clerk/nextjs";
+// import { NextResponse } from "next/server";
+
+// export default withClerkMiddleware(() => {
+//   console.log("middleware running");
+//   return NextResponse.next();
+// });
+
+// export const config = {
+//   matcher: ["/((?!_next/image|_next/static|favicon.ico).*)"],
+// };
